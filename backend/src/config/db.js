8c7481abb6ed5c9ce33227e1f05e289ea565f48c
chandrasekhar <chandrasekharsathavalli99@gmail.com>
2025-11-24 13:25:58 +0530

    fixing deployment errors

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,21 +1,16 @@
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    dialectOptions: isProduction
-      ? { ssl: { require: true, rejectUnauthorized: false } }
-      : {},
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
